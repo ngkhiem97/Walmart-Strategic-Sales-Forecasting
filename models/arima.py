d731cc0eee3663514a6eba6fa8cafc771ca54918
Khiem Nguyen <ng.khiem97vn@gmail.com>
2022-05-13 12:36:38 -0400

    update

--- models/arima.py
+++ models/arima.py
@@ -1,5 +1,16 @@
-from statsmodels.tsa.arima_model import ARIMA
+import statsmodels.api as sm
+import pandas as pd
 
-# Doing ARIMA modeling
-model = ARIMA(data, order=(2, 1, 0))
-results_ARIMA = model.fit(disp=False)
+df = pd.read_csv('../data/processed/calendar_processed-May-13-2022.csv')
+df = df["total_sales"]
+
+print("Total nulls:", df.isnull().sum())
+print("Dropping nulls...")
+df = df.dropna()
+model = sm.tsa.arima.ARIMA(df, order=(2, 1, 0))
+results_ARIMA = model.fit()
+print(results_ARIMA.summary())
+
+# Write results to new file
+with open('results/arima-results.txt', 'w') as f:
+    f.write(results_ARIMA.summary().as_text())
